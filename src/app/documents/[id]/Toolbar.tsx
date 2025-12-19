d3c6ede27882c781b70fcb643572b3bcbbfc63d4
Asad Ali <asadjattofficials@gmail.com>
2025-12-19 20:48:57 +0500

    Lists button added

--- src/app/documents/[id]/Toolbar.tsx
+++ src/app/documents/[id]/Toolbar.tsx
@@ -11,6 +11,8 @@ import {
 	ImageIcon,
 	ImagesIcon,
 	Link2Icon,
+	ListIcon,
+	ListOrderedIcon,
 	LucideIcon,
 	SearchIcon,
 	UploadIcon,
@@ -406,6 +408,53 @@ const AlignButton = () => {
 		</DropdownMenu>
 	);
 };
+
+const ListButton = () => {
+	const { editor } = useEditorStore();
+
+	const lists = [
+		{
+			label: 'Bullet List',
+			icon: ListIcon,
+			isActive: editor?.isActive('bulletList'),
+			onClick: () => editor?.chain().focus().toggleBulletList().run(),
+		},
+		{
+			label: 'Ordered List',
+			icon: ListOrderedIcon,
+			isActive: editor?.isActive('orderedList'),
+			onClick: () => editor?.chain().focus().toggleOrderedList().run(),
+		},
+	];
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<button
+					className={
+						'h-7 min-w-7 shrink-0 flex flex-col items-center  justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'
+					}
+				>
+					<ListIcon className="size-4" />
+				</button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+				{lists.map(({ label, icon: Icon, isActive, onClick }) => (
+					<button
+						key={label}
+						className={cn(
+							'flex items-center gap-x-2 px-2 py-1 hover:bg-neutral-200/80',
+							isActive && 'bg-neutral-200/80'
+						)}
+						onClick={onClick}
+					>
+						<Icon className="size-4" />
+						<span className="text-sm">{label}</span>
+					</button>
+				))}
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
 const Toolbar = () => {
 	const { editor } = useEditorStore();
 	const sections: {
@@ -501,6 +550,7 @@ const Toolbar = () => {
 			<LinkButton />
 			<ImageButton />
 			<AlignButton />
+			<ListButton />
 			<Separator orientation="vertical" className="h-6 bg-neutral-300" />
 			{sections[2].map(tool => (
 				<ToolbarButton key={tool.label} {...tool} />
