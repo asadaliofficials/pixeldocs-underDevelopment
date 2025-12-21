90144bb8525db65507b0d3ebc7aa5c4895b71d8d
Asad Ali <asadjattofficials@gmail.com>
2025-12-21 22:36:19 +0500

    implemented clerk authentication and setup convex db

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+	providers: [
+		{
+			domain: 'https://ample-fly-51.clerk.accounts.dev',
+			applicationID: 'convex',
+		},
+	],
+};
---
+++ src/components/ConvexClientProvider.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { ConvexReactClient, Authenticated, Unauthenticated, AuthLoading } from 'convex/react';
+import { ReactNode } from 'react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { ClerkProvider, useAuth, SignIn } from '@clerk/clerk-react';
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
+	return (
+		<ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+			<ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+				<Authenticated>{children}</Authenticated>
+				<Unauthenticated>
+					<div className='flex flex-col justify-center items-center min-h-screen'>
+						<SignIn />
+					</div>
+				</Unauthenticated>
+				<AuthLoading>
+					<p>Loading...</p>
+				</AuthLoading>
+			</ConvexProviderWithClerk>
+		</ClerkProvider>
+	);
+}
