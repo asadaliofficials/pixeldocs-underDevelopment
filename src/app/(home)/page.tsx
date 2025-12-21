8a82d6a6343717e8fb6a8a28a9c30c6a3f72f261
Asad Ali <asadjattofficials@gmail.com>
2025-12-21 21:33:34 +0500

    added document template and setup convex db

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import React from 'react';
 import Navbar from './Navbar';
+import TemplatesGallery from './TemplatesGallery';
 
 const HomePage = () => {
 	return (
@@ -9,11 +10,8 @@ const HomePage = () => {
 			<div className='fixed top-0 left-0 right-0 z-10 h-16 p-4 bg-white'>
 				<Navbar />
 			</div>
-			<div className='mt-16 flex flex-col justify-center items-center'>
-				<h1 className='text-4xl'>Home Page</h1>
-				<Button>
-					<Link href={'/documents/test'}>Edit Document</Link>
-				</Button>
+			<div className='mt-16'>
+				<TemplatesGallery />
 			</div>
 		</div>
 	);
---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+	{
+		id: 'blank',
+		lable: 'Blank Document',
+		imageUrl: '/blank-document.svg',
+	},
+	{
+		id: 'software-proposal',
+		lable: 'Software Development Proposal',
+		imageUrl: '/software-proposal.svg',
+	},
+	{
+		id: 'project-proposal',
+		lable: 'Project Proposal',
+		imageUrl: '/project-proposal.svg',
+	},
+	{
+		id: 'business-letter',
+		lable: 'Business Letter',
+		imageUrl: '/business-letter.svg',
+	},
+	{
+		id: 'resume',
+		lable: 'Resume',
+		imageUrl: '/resume.svg',
+	},
+	{
+		id: 'cover-letter',
+		lable: 'Cover Letter',
+		imageUrl: '/cover-letter.svg',
+	},
+	{
+		id: 'letter',
+		lable: 'Letter',
+		imageUrl: '/letter.svg',
+	},
+];
