82218aee2106c1e2ff922e7ac3eef2d2a6cbefab
Asad Ali <message.asadali@gmail.com>
2025-12-21 16:20:01 +0500

    added rurals to edit margins of page

--- src/app/documents/[id]/Editor.tsx
+++ src/app/documents/[id]/Editor.tsx
@@ -22,11 +22,13 @@ import { FontSizeExtension } from '@/extensions/font-size';
 import { LineHeightExtension } from '@/extensions/line-height';
 
 import { useEditorStore } from '@/store/use-editor-store';
+import { Ruler } from './Ruler';
 
 const Editor = () => {
 	const { setEditor } = useEditorStore();
 
 	const editor = useEditor({
+		immediatelyRender: false,
 		editorProps: {
 			attributes: {
 				class:
@@ -88,6 +90,7 @@ const Editor = () => {
 	});
 	return (
 		<div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+			<Ruler />
 			<div className="min-w-max flex justify-center w-204 py-4 print:py-0 mx-auto print:w-full print:min-w-0">
 				<EditorContent editor={editor} />
 			</div>
