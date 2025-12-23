1782f9fc7f9941f00892b44efb684d24d026cfe6
Asad Ali <message.asadali@gmail.com>
2025-12-23 18:11:43 +0500

    added pagination for douments home page

--- src/app/(home)/DocumentsTable.tsx
+++ src/app/(home)/DocumentsTable.tsx
@@ -11,6 +11,7 @@ import {
 	TableRow,
 } from '@/components/ui/table';
 import DocumentRow from './DocumentRow';
+import { Button } from '@/components/ui/button';
 
 interface Props {
 	documents: Doc<'documents'>[] | undefined;
@@ -21,31 +22,31 @@ interface Props {
 const DocumentsTable = ({ documents, status, loadMore }: Props) => {
 	if (status === 'LoadingFirstPage') {
 		return (
-			<div className='flex justify-center items-center h-24'>
-				<LoaderIcon className='animate-spin text-muted-foreground size-5' />
+			<div className="flex justify-center items-center h-24">
+				<LoaderIcon className="animate-spin text-muted-foreground size-5" />
 			</div>
 		);
 	}
 	return (
-		<div className='max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5'>
+		<div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5">
 			{documents === undefined ? (
-				<div className='flex justify-center items-center h-24'>
-					<LoaderIcon className='animate-spin text-muted-foreground size-5' />
+				<div className="flex justify-center items-center h-24">
+					<LoaderIcon className="animate-spin text-muted-foreground size-5" />
 				</div>
 			) : (
 				<Table>
 					<TableHeader>
-						<TableRow className='hover:bg-transparent border-none'>
+						<TableRow className="hover:bg-transparent border-none">
 							<TableHead>Name</TableHead>
 							<TableHead>&nbsp;</TableHead>
-							<TableHead className='hidden md:table-cell'>Shared</TableHead>
-							<TableHead className='hidden md:table-cell'>Created at</TableHead>
+							<TableHead className="hidden md:table-cell">Shared</TableHead>
+							<TableHead className="hidden md:table-cell">Created at</TableHead>
 						</TableRow>
 					</TableHeader>
 					{documents.length === 0 ? (
 						<TableBody>
-							<TableRow className='hover:bg-transparent border-none'>
-								<TableCell colSpan={4} className='h-24 text-center text-muted-foreground'>
+							<TableRow className="hover:bg-transparent border-none">
+								<TableCell colSpan={4} className="h-24 text-center text-muted-foreground">
 									No documents found
 								</TableCell>
 							</TableRow>
@@ -59,6 +60,16 @@ const DocumentsTable = ({ documents, status, loadMore }: Props) => {
 					)}
 				</Table>
 			)}
+			<div className="flex items-center justify-center">
+				<Button
+					variant={'ghost'}
+					size={'sm'}
+					onClick={() => loadMore(10)}
+					disabled={status !== 'CanLoadMore'}
+				>
+					{status === 'CanLoadMore' ? 'Load more' : 'End of Results'}
+				</Button>
+			</div>
 		</div>
 	);
 };
