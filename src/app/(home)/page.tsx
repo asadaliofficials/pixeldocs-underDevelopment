9af9ea5a537803ce35b10123b2096756f64a4201
Asad Ali <message.asadali@gmail.com>
2025-12-23 18:27:18 +0500

    added Search functionality for documents

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,19 +5,21 @@ import TemplatesGallery from './TemplatesGallery';
 import { api } from '../../../convex/_generated/api';
 import { usePaginatedQuery } from 'convex/react';
 import DocumentsTable from './DocumentsTable';
+import { useSearchParams } from '@/hooks/use-search-params';
 
 const HomePage = () => {
+	const [search] = useSearchParams();
 	const { results, status, loadMore } = usePaginatedQuery(
 		api.documents.get,
-		{},
+		{ search },
 		{ initialNumItems: 10 }
 	);
 	return (
-		<div className='min-h-screen flex flex-col'>
-			<div className='fixed top-0 left-0 right-0 z-10 h-16 p-4 bg-white'>
+		<div className="min-h-screen flex flex-col">
+			<div className="fixed top-0 left-0 right-0 z-10 h-16 p-4 bg-white">
 				<Navbar />
 			</div>
-			<div className='mt-16'>
+			<div className="mt-16">
 				<TemplatesGallery />
 				<DocumentsTable documents={results} status={status} loadMore={loadMore} />
 			</div>
