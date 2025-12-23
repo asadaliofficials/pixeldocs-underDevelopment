33f4d92ccd5e6b09c0cab4dddd65f3e68e79a56b
Asad Ali <message.asadali@gmail.com>
2025-12-23 18:00:26 +0500

    Added rename functionality for document

--- convex/documents.ts
+++ convex/documents.ts
@@ -37,7 +37,7 @@ export const removeById = mutation({
 		}
 
 		const document = await ctx.db.get(args.id);
-		if (!document) { 
+		if (!document) {
 			throw new ConvexError('Document not found');
 		}
 
@@ -46,5 +46,26 @@ export const removeById = mutation({
 		}
 
 		return await ctx.db.delete(args.id);
+	},
+});
+
+export const updateById = mutation({
+	args: { id: v.id('documents'), title: v.string() },
+	handler: async (ctx, args) => {
+		const user = await ctx.auth.getUserIdentity();
+		if (!user) {
+			throw new ConvexError('Unauthorized');
+		}
+
+		const document = await ctx.db.get(args.id);
+		if (!document) {
+			throw new ConvexError('Document not found');
+		}
+
+		if (document.ownerId !== user.subject) {
+			throw new ConvexError('Unauthorized');
+		}
+
+		return await ctx.db.patch(args.id, { title: args.title });
 	},
 });
--- src/app/(home)/DocumentMenu.tsx
+++ src/app/(home)/DocumentMenu.tsx
@@ -6,8 +6,9 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { ExternalLinkIcon, MoreVertical, TrashIcon } from 'lucide-react';
+import { ExternalLinkIcon, FilePenIcon, MoreVertical, TrashIcon } from 'lucide-react';
 import { Id } from '../../../convex/_generated/dataModel';
+import RenameDialog from '@/components/RenameDialog';
 
 interface props {
 	documentId: Id<'documents'>;
@@ -18,18 +19,24 @@ const DocumentMenu = ({ documentId, title, onNewTab }: props) => {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant={'ghost'} size={'icon'} className='rounded-full'>
-					<MoreVertical className='size-4' />
+				<Button variant={'ghost'} size={'icon'} className="rounded-full">
+					<MoreVertical className="size-4" />
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent>
 				<DropdownMenuItem onClick={() => onNewTab(documentId)}>
-					<ExternalLinkIcon className='size-4 mr-2' />
+					<ExternalLinkIcon className="size-4 mr-2" />
 					Open in new tab
 				</DropdownMenuItem>
+				<RenameDialog documentId={documentId} initialTitle={title}>
+					<DropdownMenuItem onSelect={e => e.preventDefault()} onClick={e => e.stopPropagation()}>
+						<FilePenIcon className="size-4 mr-2" />
+						Rename
+					</DropdownMenuItem>
+				</RenameDialog>
 				<DeleteDialog documentId={documentId}>
 					<DropdownMenuItem onSelect={e => e.preventDefault()} onClick={e => e.stopPropagation()}>
-						<TrashIcon className='size-4 mr-2' />
+						<TrashIcon className="size-4 mr-2" />
 						Delete
 					</DropdownMenuItem>
 				</DeleteDialog>
